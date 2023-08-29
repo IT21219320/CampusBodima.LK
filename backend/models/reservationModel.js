5bbc0792e807e1baa92508ac3ba37562243c1d2b
IT21292590 <it21292590@my.sliit.lk>
2023-08-29 12:16:28 +0530

    Create ReservationModel.js

---
+++ backend/models/reservationModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const reservationSchema = mongoose.Schema({
+    boardingId: {
+        type: String, 
+        required: true,
+    },
+
+    roomNo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    occupantID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    ReservedDate: {
+        type: Date,
+        required: true
+    },
+
+    Duration: {
+        type: Date,
+        required:true
+    },
+
+    DurationUpdatedAt: {
+        type: Date,
+    }
+
+},{
+    timestamps :true
+});
+
+const Reservation =  mongoose.model('Reservation' ,reservationSchema );
+
+export default Reservation;
