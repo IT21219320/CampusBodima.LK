fe588cfb9f7448ff3327919f3fbfd774d8be666f
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2023-09-05 07:23:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <ToastContainer />
-      <Container className='mw-100 py-0 px-0' style={{position:'fixed', minHeight:'100vh', height:'100%', overflow:'auto', display:'flex', background:'aliceblue'}}>
+      <Container className='mw-100 py-0 px-0' style={{position:'fixed', minHeight:'100vh', height:'100%', overflow:'auto', display:'flex', background:'#f8f7fa'}}>
         <Outlet />
       </Container>
     </>
