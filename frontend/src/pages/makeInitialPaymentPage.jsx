d1e612fc8351a6e226bd774fde5ee4f9443ff88d
dewmina2001 <devmin24@gmail.com>
2023-09-17 14:40:05 +0530

    update makepayment

--- frontend/src/pages/makeInitialPaymentPage.jsx
+++ frontend/src/pages/makeInitialPaymentPage.jsx
@@ -1,2 +1,22 @@
-import HorizontalLinearStepper from "../components/stepper";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
+const makeInitialPaymentPage = () =>{
+    return (
+        <>
+            <Stepper>
+                <Step>
+                    <StepLabel>Payment</StepLabel>
+                </Step>
+            </Stepper>
+        </>
+    )
+}
+
+
+export default makeInitialPaymentPage;
