925184f78901a65e0b414e077c6ab6b38907b319
dewmina2001 <devmin24@gmail.com>
2023-09-17 12:56:16 +0530

    addPaymentInitilapage

---
+++ backend/models/paymentModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const paymentSchema = mongoose.Schema({
+    paymentType : {
+        type : String
+    },
+    currency : {
+        type : String
+    },
+    amount : {
+        type : String
+    },
+    description: {
+        type: String
+    },
+    room: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Room'
+    }],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    occupant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    date : {
+        type: date,
+
+    }
+    
+}, {
+    timestamps: true
+});
+
+const payment = mongoose.model('Payment', paymentSchema);
+
+export default payment
---
+++ frontend/src/pages/makeInitialPaymentPage.jsx
@@ -0,0 +1,2 @@
+import HorizontalLinearStepper from "../components/stepper";
+
