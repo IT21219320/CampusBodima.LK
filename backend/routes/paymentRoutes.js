8119391a8105126e057d4bb1a7e2e2cd5ff5940c
dewmina2001 <devmin24@gmail.com>
2023-09-19 02:15:31 +0530

    set routes

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -3,7 +3,11 @@ import express from 'express';
 
 const router = express.Router();
 
-
+router.get('/',getPath);
+router.get('/config', getPublichkey);
+router.post('/make', makePayment);
+router.route('/create-payment-intent').post(getIntent);
+router.post('/webhook', getWebHook);
 
 
 
