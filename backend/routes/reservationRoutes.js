48d55a3462287ce1b1fea15864d7eab506cc7de8
IT21292590 <it21292590@my.sliit.lk>
2023-09-21 16:19:05 +0530

    reservation crud

--- backend/routes/reservationRoutes.js
+++ backend/routes/reservationRoutes.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import { reserveRoom,
+         updateDuration, 
+         getMyReservation, 
+         getBoardingReservations, 
+         getPendingReservations,
+         approvePendingStatus,
+         deletePendingStatus,
+         deleteReservation 
+    } from '../controllers/reservationController.js';
  
 
 const router = express.Router();
 
-
-
-
+router.post('/bookRoom', reserveRoom);
+router.put('/updateDuration', updateDuration)
+router.get('/MyRoom', getMyReservation);
+router.get('/veiwReservations',getBoardingReservations);
+router.get('/pending' ,getPendingReservations);
+router.put('/aprovePending', approvePendingStatus);
+router.delete('/deletePending', deletePendingStatus);
+router.delete('daleteReservation' , deleteReservation);
 
 export default router;
