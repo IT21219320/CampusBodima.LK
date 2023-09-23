85fa058f154d04b4c2b66f71c9cd1382ebb71eb1
IT21292590 <it21292590@my.sliit.lk>
2023-09-23 08:03:34 +0530

    reservation Form

---
+++ frontend/src/pages/pendingReservationsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PendingReservationsPage = () => { 
+  return (
+    <div>
+      <h1>Helloooo</h1>
+    </div>
+  );
+};
+
+export default PendingReservationsPage;
