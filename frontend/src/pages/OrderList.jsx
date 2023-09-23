97bcc6113cf0f0f3363706e56dbcc33bda02f570
IT21302008 <it21302008@my.sliit.lk>
2023-09-23 10:50:48 +0530

    css

--- frontend/src/pages/OrderList.jsx
+++ frontend/src/pages/OrderList.jsx
@@ -1,107 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { Breadcrumbs, Typography, Fade, Card, CardContent, Link, InputLabel, Select, MenuItem, FormControl, TextField,} from '@mui/material';
-import Sidebar from '../components/sideBar';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-   const OrderList = () => {
-     const [orders, setOrders] = useState([]);
+const OrderList = () => {
+  const [orders, setOrders] = useState([]);
 
-     useEffect(() => {
-       const fetchOrders = async () => {
-         try {
-           const response = await fetch('/api/orders/all');
-           const data = await response.json();
-           setOrders(data);
-         } catch (error) {
-           console.error('Error fetching orders:', error);
-         }
-       };
+  useEffect(() => {
+    
+    axios.get("/api/orders")
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching orders:", error);
+      });
+  }, []);
 
-       fetchOrders();
-     }, []);
-     const foods = [
-      {
-        _id: 1,
-        date: '2023.08.25',
-        orderno:1,
-        product: 'Rice & curry',
-        foodType: 'Chicken',
-        quantity: 2,
-        price: 350,
-        total:700,
-        status:'ready',
-      },
-      {
-        _id: 2,
-        date: '2023.08.13',
-        orderno:2,
-        product: 'Fried Rice',
-        foodType: 'Chicken',
-        quantity: 1,
-        price: 450,
-        total:450,
-        status:'pending...',
-      },
-    ];
-  
-
-     return (
-       <div>
-         <h2 style={{ fontSize: '24px', color: 'blue'}}>My Orders</h2>
-         
-         <ul>
-          <p></p>
-          
-         </ul>
-         <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-         <tr>
-            <th style={tableHeaderStyle}>Date</th>
-            <th style={tableHeaderStyle}>Order Number</th>
-            <th style={tableHeaderStyle}>Order Details</th>
-            <th style={tableHeaderStyle}>Order Total</th>
-            <th style={tableHeaderStyle}>Status</th>
-          </tr>
-        {foods.map((food) => (
+  return (
+    <>
+      <h1>Order List</h1>
+      <table>
+        <thead>
           <tr>
-            <td style={tableCellStyle}key={food._id}>
-             {food.date}
-          </td>
-          <td style={tableCellStyle}key={food._id}>
-          {food.orderno}
-          </td>
-          <td style={tableDetailsStyle}key={food._id}>
-          {food.product}{'\t'}{food.foodType}{'\t'}{food.quantity}
-          </td>
-          <td style={tableCellStyle}key={food._id}>
-             {food.total}
-          </td>
-          <td style={tableCellStyle}key={food._id}>
-             {food.status}
-          </td>
+            <th>Product</th>
+            <th>Order No</th>
+            <th>Status</th>
+            <th>Date</th>
+            <th>Total</th>
           </tr>
-          
-        ))}
-        
-        </table>
-       </div>
-     );
-   };
-   const tableHeaderStyle = {
-    backgroundColor: '#333',
-    color: '#fff',
-    padding: '10px',
-    textAlign: 'left',
-  };
-  const tableCellStyle = {
-  
-    border: '1px solid #ccc',
-    padding: '10px',
-    textAlign: 'center',
-  };
-  const tableDetailsStyle = {
-  
-    border: '1px solid #ccc',
-    padding: '10px',
-    textAlign: 'justify',
-  };
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order._id}>
+              <td>{order.product}</td>
+              <td>{order.orderNo}</td>
+              <td>{order.status}</td>
+              <td>{new Date(order.date).toLocaleDateString()}</td>
+              <td>{order.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-   export default OrderList;
+export default OrderList;
