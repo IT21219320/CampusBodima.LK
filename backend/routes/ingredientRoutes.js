5e2fb3e1e2a5c52af10891628dc3ccfd058abee6
Janul <it21240188@my.sliit.lk>
2023-10-17 13:07:15 +0530

    inroute

--- backend/routes/ingredientRoutes.js
+++ backend/routes/ingredientRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import {addIngredient,getBoardingIngredient,getBoardingIngredientNames,getOwnerBoarding,updateIngredient,getUpdateIngredients,deleteIngredient,increaseIngredientQuantity,reduceIngredientQuantity,getIngredientHistoy} from '../controllers/ingredientController.js'; 
+import {addIngredient,getBoardingIngredient,getBoardingIngredientNames,getOwnerBoarding,updateIngredient,getUpdateIngredients,deleteIngredient,increaseIngredientQuantity,reduceIngredientQuantity,getIngredientHistoy,getKitchenUsersEmails,addKitchenUser} from '../controllers/ingredientController.js'; 
 
 const router = express.Router();
 
 router.post('/add', addIngredient);
+router.post('/add/manager', addKitchenUser);
 router.post('/owner/ingredients', getBoardingIngredient);
 router.post('/owner/ingredients/increase', increaseIngredientQuantity);
 router.post('/owner/ingredients/reduce', reduceIngredientQuantity);
 router.post('/owner/ingredients/names', getBoardingIngredientNames);
+router.post('/manager/ingredients/emails', getKitchenUsersEmails);
 router.get('/owner/:ownerId', getOwnerBoarding);
 router.put('/owner', updateIngredient);
 router.get('/owner/update/:boardingId/:ingredientId', getUpdateIngredients);
