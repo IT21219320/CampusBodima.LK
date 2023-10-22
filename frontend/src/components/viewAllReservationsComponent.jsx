0e3d98928764750ff7d6a1efd694ee3c1c7b93fc
IT21292590 <it21292590@my.sliit.lk>
2023-10-22 11:35:09 +0530

    added filter to reservations

--- frontend/src/components/viewAllReservationsComponent.jsx
+++ frontend/src/components/viewAllReservationsComponent.jsx
@@ -14,6 +14,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import { GridViewRounded, GetAppRounded } from '@mui/icons-material';
 import { useGetBoardingReservationsMutation } from '../slices/reservationsApiSlice';
 import { useGetBoardingBybIdMutation } from '../slices/reservationsApiSlice';
 import { useParams } from 'react-router-dom';
@@ -41,7 +43,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 const ViewAllReservations = ({ bId }) => {
 
   const searchStyle = {
-    width: '29%',
+    width: '55%',
     borderRadius: '20px',
     padding: '6px',
     paddingLeft: '18px',
@@ -56,7 +58,9 @@ const ViewAllReservations = ({ bId }) => {
   const [reservations, setReservations] = useState([]);
   const [boardingId, setBoardingId] = useState('');
   const [searchWord, setSearchWord] = useState('');
+  const [month, setMonth] = useState('');
   const [loading, setLoading] = useState(true);
+
 
   const handleSearch = (e) => {
     setSearchWord(e.target.value);
@@ -67,20 +71,52 @@ const ViewAllReservations = ({ bId }) => {
       reservation.lastName.toLowerCase().includes(searchWord.toLowerCase());
   });
 
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value);
+  };
+
+  //function implementation of getting the month and returning sorted data
+  const sortReservationsByREservedMonth = (reservations, selectedMonth) => {
+    return reservations.filter(reservation => {
+      const reservedDate = new Date(reservation.Date);
+      const reservedMonth = reservedDate.toLocaleString('default', { month: 'short' });
+      if (selectedMonth === "") {
+        return reservedMonth
+      } else {
+        return reservedMonth === selectedMonth
+      };
+    });
+  }
+
+  //calling the function
+  const sortedReservationsByMonth = sortReservationsByREservedMonth(filteredReservations, month);
+
+
   const loadData = async () => {
+
     try {
       setLoading(true)
 
       const res = await getReservation({ boardingId: bId }).unwrap();
-      const resbor = await getBording({ bId: bId })
       setReservations(res);
-      setBoardingId(resbor.data.selectedBoarding);
-      console.log(resbor.data.selectedBoarding);
+
       setLoading(false)
 
     } catch (error) {
       setLoading(false)
+      console.error('Error getting reservations', error);
+    }
+
+    try {
+      const resbor = await getBording({ bId: bId });
+
+      setBoardingId(resbor.data.selectedBoarding);
+      console.log(resbor.data.selectedBoarding);
+    } catch (error) {
+
       console.error('Error getting boardings', error);
+
     }
 
   }
@@ -91,19 +127,64 @@ const ViewAllReservations = ({ bId }) => {
 
   return (
     <>
-      <div style={{ marginTop: '17px' }}>
-        <input id="outlined-search"
-          type="search"
-          placeholder="Search occupant name..."
-          style={searchStyle}
-          value={searchWord}
-          onChange={handleSearch}
-        />
+      <Row>
 
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-      </div>
+        <Col>
+
+          <div style={{ marginTop: '17px' }}>
+            <input id="outlined-search"
+              type="search"
+              placeholder="Search occupant name..."
+              style={searchStyle}
+              value={searchWord}
+              onChange={handleSearch}
+            />
+
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+          </div>
+
+        </Col>
+
+        <Col>
+          <div style={{ width: '130px', float: 'left', marginTop: '17px', marginLeft: '200px' }}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth size='small'>
+                <InputLabel id="demo-simple-select-label">Month</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={month}
+                  label="Month"
+                  onChange={handleMonthChange}
+                >
+                  <MenuItem value="" >All</MenuItem>
+                  <MenuItem value="Jan">January</MenuItem>
+                  <MenuItem value="Feb">February</MenuItem>
+                  <MenuItem value="Mar">March</MenuItem>
+                  <MenuItem value="Apr">April</MenuItem>
+                  <MenuItem value="May">May</MenuItem>
+                  <MenuItem value="Jun">June</MenuItem>
+                  <MenuItem value="Jul">July</MenuItem>
+                  <MenuItem value="Aug">August</MenuItem>
+                  <MenuItem value="Sep">September</MenuItem>
+                  <MenuItem value="Oct">October</MenuItem>
+                  <MenuItem value="Nov">November</MenuItem>
+                  <MenuItem value="Dec">December</MenuItem>
+
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+
+          <div>
+            <Button variant="contained" style={{ background: '#4c4c4cb5', float: 'right', marginTop: '17px' }} /*onClick={exportToPDF} */>Export<GetAppRounded /></Button>
+          </div>
+
+        </Col>
+      </Row>
+
       {loading ? (
         <>
           <div style={{ width: '100%', height: '60vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
@@ -135,38 +216,62 @@ const ViewAllReservations = ({ bId }) => {
 
                 {reservations.length > 0 ? (
                   <>
-                    {filteredReservations.length > 0 ? (<>{filteredReservations.map((reservation) => (
-                      <TableRow
-                        key={reservation.Id}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell component="th" scope="row">
-                          {reservation.Id}
-                        </TableCell>
+                    {filteredReservations.length > 0 ? (
 
-                        <TableCell align="right">{reservation.firstName}</TableCell>
-                        <TableCell align="right">{reservation.lastName}</TableCell>
-                        <TableCell align="right">{reservation.email}</TableCell>
-                        <TableCell align="right">{new Date(reservation.Date).toDateString()}</TableCell>
-                        <TableCell align="right">{reservation.Duration}</TableCell>
-                        {reservation.bType === 'Hostel' && (
-                          <TableCell align="right">{reservation.RoomNo}</TableCell>
-                        )}
+                      <>
+                        {sortedReservationsByMonth.length > 0 ? (
+                          <>
+                            {sortedReservationsByMonth.map((reservation) => (
+                              <TableRow
+                                key={reservation.Id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell component="th" scope="row">
+                                  {reservation.Id}
+                                </TableCell>
 
-                      </TableRow>
-                    ))
-                    }</>) : (<> <TableRow style={{ color: 'rgb(255, 255, 255)' }}>
-                      <TableCell align="right">   </TableCell>
-                      <TableCell align="right">   </TableCell>
-                      <TableCell align="right">   </TableCell>
-                      <TableCell style={{ fontSize: '20px', fontFamily: 'cursive', color: '#64651d' }}>No Reservations to display</TableCell>
-                      <TableCell align="right">   </TableCell>
-                      <TableCell align="right">   </TableCell>
-                      <TableCell align="right">   </TableCell>
+                                <TableCell align="right">{reservation.firstName}</TableCell>
+                                <TableCell align="right">{reservation.lastName}</TableCell>
+                                <TableCell align="right">{reservation.email}</TableCell>
+                                <TableCell align="right">{new Date(reservation.Date).toDateString()}</TableCell>
+                                <TableCell align="right">{reservation.Duration}</TableCell>
+                                {reservation.bType === 'Hostel' && (
+                                  <TableCell align="right">{reservation.RoomNo}</TableCell>
+                                )}
 
-                    </TableRow>
-                    </>)}
-                  </>) : (<>
+                              </TableRow>
+                            ))
+                            }
+
+                          </>) : (
+                          <>
+                            <TableRow>
+                              <TableCell align="right">   </TableCell>
+                              <TableCell align="right">   </TableCell>
+                              <TableCell align="right">   </TableCell>
+                              <TableCell style={{ fontSize: '20px', fontFamily: 'cursive', color: '#64651d' }}>No Reservations to display</TableCell>
+                              <TableCell align="right">   </TableCell>
+                              <TableCell align="right">   </TableCell>
+                              <TableCell align="right">   </TableCell>
+
+                            </TableRow>
+
+                          </>)}
+                      </>) : (
+                      <>
+                        <TableRow style={{ color: 'rgb(255, 255, 255)' }}>
+                          <TableCell align="right">   </TableCell>
+                          <TableCell align="right">   </TableCell>
+                          <TableCell align="right">   </TableCell>
+                          <TableCell style={{ fontSize: '20px', fontFamily: 'cursive', color: '#64651d' }}>No Reservations to display</TableCell>
+                          <TableCell align="right">   </TableCell>
+                          <TableCell align="right">   </TableCell>
+                          <TableCell align="right">   </TableCell>
+
+                        </TableRow>
+                      </>)}
+                  </>) :
+                  (<>
                     <TableRow>
                       <TableCell align="right">   </TableCell>
                       <TableCell align="right">   </TableCell>
