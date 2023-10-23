1d1b9415fe6d08d6d37ed93878a685f1b94c0814
Janul <it21240188@my.sliit.lk>
2023-10-23 11:45:01 +0530

    reduceInventory

--- frontend/src/components/reduceinventoryComponent.jsx
+++ frontend/src/components/reduceinventoryComponent.jsx
@@ -150,9 +150,9 @@ const ReduceinventoryPage = ({ boardingId }) => {
                               ></Form.Control>
                             </Form.Group>
 
-                             
+                            <Form.Label>Measurement</Form.Label> 
                             <Box sx={{ minWidth: 120 }}>
-                              <FormControl fullWidth>
+                              <FormControl fullWidth size='small'>
                                   <InputLabel id="demo-simple-select-label">Measurement</InputLabel>
                                   <Select
                                     labelId="demo-simple-select-label"
